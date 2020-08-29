ba238e6b5a2cfda6c67a5633b8091b59c2113905
AdityaChuck <adityanarayanmahapatra00@gmail.com>
2020-08-30 02:41:22 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+	import PollList from './components/PollList.svelte'
+	import CreatePollForm from './components/CreatePollForm.svelte'
+	import Tabs from './shared/Tabs.svelte'
+
+	//tabs
+	let items = ['Current Polls', 'Add New Poll']
+	let activeItem = 'Current Polls'
+
+	const tabChange = e => {
+		activeItem = e.detail
+	} 
+
+
+	const onAdd = e => {
+		activeItem = 'Current Polls'
+	}
+
+	
+
+</script>
+
+<Header/>
+<main>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<PollList />
+	{:else if activeItem === 'Add New Poll'}
+		<CreatePollForm on:add={onAdd} />
+	{/if}
+		
+</main>
+<Footer/>
+<style>
+	main{
+		max-width: 960p;
+		margin: 40px;
+	}
+</style>
---
+++ src/components/PollList.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { fade, slide, scale } from 'svelte/transition'
+    import { flip } from 'svelte/animate'
+    import PollStore from '../stores/PollStore.js'
+    import PollDetails from './PollDetails.svelte'
+  
+</script>
+
+<div class="poll-list">
+    { #if $PollStore.length !== 0 }
+        {#each $PollStore as poll (poll.id)}
+            <div in:fade out:scale|local animate:flip={{duration: 500}} >
+                <PollDetails  {poll} />
+            </div>
+        {/each}
+    {/if}
+</div>
+
+<style>
+    .poll-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+</style>
